eabc792f5036c4588e617f464317e1639546b53f
Mike English <mike.english@atomicobject.com>
2015-01-09 17:38:31 -0500

    Add random number generation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use std::rand;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = (rand::random::<uint>() % 100u) + 1u;
+
+    println!("The secret nymber is: {}", secret_number);
 
     println!("Please input your guess");
 
