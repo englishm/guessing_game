1a42626a78353ab0edbbf2d40e6a933c4975515b
Mike English <mike.english@atomicobject.com>
2015-01-09 18:14:26 -0500

    Add matching, parse number from input.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::rand;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -13,6 +14,27 @@ fn main() {
     let input = io::stdin().read_line()
                            .ok()
                            .expect("Failed to read line");
+    let input_num: Option<uint> = input.trim().parse();
 
-    println!("You guessed: {}", input);
+    let num = match input_num {
+      Some(num) => num,
+      None      => {
+          println!("Please input a number!");
+          return;
+      }
+    };
+
+    println!("You guessed: {}", num);
+
+    match cmp(num, secret_number) {
+      Ordering::Less      => println!("Too small!"),
+      Ordering::Greater   => println!("Too big!"),
+      Ordering::Equal     => println!("You win!"),
+    }
+}
+
+fn cmp(a: uint, b: uint) -> Ordering {
+  if a < b { Ordering::Less }
+  else if a > b { Ordering::Greater }
+  else { Ordering::Equal }
 }
