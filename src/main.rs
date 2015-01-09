7a06426ff98706f66a53fbf34aff345d8c9c0266
Mike English <mike.english@atomicobject.com>
2015-01-09 17:33:28 -0500

    First example code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess");
+
+    let input = io::stdin().read_line()
+                           .ok()
+                           .expect("Failed to read line");
+
+    println!("You guessed: {}", input);
 }
